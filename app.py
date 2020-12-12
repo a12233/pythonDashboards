e900126274f33a3d6f0a33ec06b64582a774f405
Rex Li <rexli@Rexs-MacBook-Pro.local>
2020-12-11 22:12:35 -0500

    display json, test pipeline

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 import streamlit as st
+from polygon import RESTClient
+import os
+import json
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+def testPolygon(ticker): 
+    key = os.environ['POLYGON_KEY']
+
+    with RESTClient(key) as client:
+
+        response = client.reference_stock_financials(ticker, limit=1, type="Y")
+        attribute = ['ticker','revenuesUSD', 'marketCapitalization', 'grossProfit', 'netCashFlowFromOperations']
+        res = dict.fromkeys(attribute)
+        for i in attribute: 
+            res[i] = response.results[0][i]
+        return json.dumps(res) 
+
+if __name__ == "__main__":
+    st.json(testPolygon('AAPL'))
+    st.json(testPolygon('AMZN'))
