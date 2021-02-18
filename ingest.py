815c92226a6b161073fb5d2c49c8ba8ea5cbb028
Rex Li <rexli@Rexs-MacBook-Pro.local>
2021-02-17 19:52:56 -0500

    share db connection

--- ingest.py
+++ ingest.py
@@ -8,14 +8,12 @@ import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
 import warnings
 
-def download(v):
+def download(v, conn):
     v = v[1]
     if "^" in v:
         return
     v = v.replace("/","-")
-    conn = sqlite3.connect('prices.db')
-    c = conn.cursor()
-    data = yf.Ticker(v).history(period="day")
+    data = yf.Ticker(v).history(period="max")
     data.to_sql(v, conn, schema=None, if_exists='replace', index=True, index_label=None, chunksize=None, dtype=None, method=None)
 
 
@@ -35,7 +33,10 @@ if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     n_threads = 20
     df = pd.read_csv('./tickers.csv')
+    tickerLen = len(df['Symbol'].index)
     ticker = df['Symbol'].iteritems()
+    conn = sqlite3.connect('prices.db')
+
     with ThreadPoolExecutor(max_workers=n_threads) as pool:
-        pool.map(download, ticker)
+        pool.map(download, ticker, [conn] * tickerLen)
     upload(s3_client, S3_BUCKET)
